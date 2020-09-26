06b4c7b9a3e8af1f06458253e9cdd8ce9d9b4814
Nicolas Ramos Gomez (L) <nicorg426@gmail.com>
2020-09-26 00:19:34 -0400

    added comments

--- bot/app.ts
+++ bot/app.ts
@@ -2,21 +2,10 @@ import  Discord from 'discord.js';
 import secrets from './secrets.json';
 
 const client = new Discord.Client();
+
 //deepai
-
-import  deepai from 'deepai'; // OR include deepai.min.js as a script tag in your HTML
-
+import  deepai from 'deepai'; 
 deepai.setApiKey(secrets.apiToken);
-
-const getImage = async (imgurl, callback:Function) => {
-    let resp = await deepai.callStandardApi("toonify", {
-            image: imgurl, 	
-	});
-
-	callback(resp);
-}
-
-
 
 //discord bot 
 client.on('message', message => {
@@ -24,7 +13,7 @@ client.on('message', message => {
 	console.log(message.attachments);
 
 	if (message.content === '!status') {
-		message.channel.send('Aasdlive');
+		message.channel.send('Alive');
 	}
 	
 	if( message.attachments.size !== 0 ){
@@ -40,10 +29,6 @@ client.on('message', message => {
 			})
 		}
 	}
-	
-
-	
-
 });
 
 
@@ -55,22 +40,38 @@ client.once('ready', () => {
 client.login(secrets.clientToken);
 
 
+//Function to get image
+const getImage = async (imgurl, callback:Function) => {
+    let resp = await deepai.callStandardApi("toonify", {
+            image: imgurl, 	
+	});
+
+	callback(resp);
+}
+
+
+//accepted image types
 const imgTypes:Array<String> = ["gif", "png", "jpeg", "jpg"];
 
+
+/**
+ * Maps an attachments json to a smaller json (probably not needed)
+ * @param msg 
+ */
 const mapAttachment = (msg) =>{
 	const attachments = msg;
 	const obj = {
 		name : attachments.name,
 		url : attachments.url
 	}
-
 	console.log(obj.url + " " + obj.name+"  attachements object");
-
 	return obj; 
-
-
 }
 
+/**
+ * Checks to see if a url is to an image
+ * @param url url in question
+ */
 const isImageFile = (url:String)=>{
 	
 	const type:String =  url.split(".").slice(-1)[0];
